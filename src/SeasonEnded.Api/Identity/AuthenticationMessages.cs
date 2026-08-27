a3966917fa00d2ad7a5fcab696cdf72d70c26216
Iker Obregon <iobregon@plainconcepts.com>
2026-08-27 17:44:32 +0200

    refactor(identity): clarify localized messages

--- src/SeasonEnded.Api/Identity/AuthenticationMessages.cs
+++ src/SeasonEnded.Api/Identity/AuthenticationMessages.cs
@@ -2,16 +2,21 @@ namespace SeasonEnded.Api.Identity;
 
 public static class AuthenticationMessages
 {
-    public static EmailMessage MagicLink(string? language, string token) =>
-        language == "es"
-            ? new EmailMessage(
+    public static EmailMessage MagicLink(string? language, string token)
+    {
+        if (language == "es")
+        {
+            return new EmailMessage(
                 "",
                 "Inicia sesion en Season Ended",
                 $"Haz clic aqui para iniciar sesion: {token}",
-                $"<p>Haz clic aqui para iniciar sesion: <code>{token}</code></p>")
-            : new EmailMessage(
-                "",
-                "Sign in to Season Ended",
-                $"Click here to sign in: {token}",
-                $"<p>Click here to sign in: <code>{token}</code></p>");
+                $"<p>Haz clic aqui para iniciar sesion: <code>{token}</code></p>");
+        }
+
+        return new EmailMessage(
+            "",
+            "Sign in to Season Ended",
+            $"Click here to sign in: {token}",
+            $"<p>Click here to sign in: <code>{token}</code></p>");
+    }
 }
